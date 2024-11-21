c3af48a53b64c8208edd0517287322c51eb9cacf
gonu <90233723+makisepel@users.noreply.github.com>
2024-11-21 09:30:40 +0900

    feat: Initialize and Add each pages

---
+++ src/pages/List/ListPage.jsx
@@ -0,0 +1,12 @@
+import './ListPage.css';
+
+const ListPage = () => {
+    return (
+    <div className="container">
+        <h1 className="title">ListPage</h1>
+        <p className="description">ListPage</p>
+    </div>
+    );
+}
+
+export default ListPage;
---
+++ src/pages/Map/MapPage.jsx
@@ -0,0 +1,12 @@
+import './MapPage.css';
+
+const MapPage = () => {
+    return (
+    <div className="container">
+        <h1 className="title">MapPage</h1>
+        <p className="description">MapPage</p>
+    </div>
+    );
+}
+
+export default MapPage;
