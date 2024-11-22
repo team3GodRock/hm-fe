f5aeccf8bcace6d5899646bb5f7edac3ab498082
gonu <90233723+makisepel@users.noreply.github.com>
2024-11-23 02:04:00 +0900

    feat: Add components for ProfilePage and global

--- src/pages/Profile/ProfilePage.jsx
+++ src/pages/Profile/ProfilePage.jsx
@@ -1,21 +1,110 @@
 import SideBar from '../../components/Sidebar';
 import './ProfilePage.css';
 import useNav from '../../hooks/useNav';
+import styled from 'styled-components';
+import { ContainerBox, MainBox, PageBox, HeaderWrapper, HorizontalDivider, ImageWrapper } from '../../styles/globalstyles';
+
+
+const HeadProfile = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 24px;
+    padding: 0 20px;
+`;
+
+const TextContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    padding: 15px 10px;
+    align-items: center;
+`;
+
+const NameContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    align-items: flex-start;
+    justify-content: flex-start;
+    padding: 48px 16px;
+`;
+
+const TextPosition = styled.div`
+    display: flex;
+    font-size: 36px;
+    color: #3E6AF2;
+`;
+
+const TextName = styled.div`
+    display: flex;
+    font-size: 64px;
+    font-weight: 600;
+`;
+
+const ResumeContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    padding: 0 20px;
+`;
+
+const FormContainer = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    gap: 20px;
+    padding: 0 20px;
+`;
+
+const FormResume = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+`;
 
 
 const ProfilePage = () => {
     const { navMenus, navSubMenus } = useNav();
 
     return (
-    <>
+    <PageBox>
         <SideBar
         navMenus={navMenus}
-        navSubMenus={navSubMenus}
-        title='정치인'/>
-        <div className="profile-container">
-            <h1>Profile Page</h1>
-        </div>
-    </>
+        navSubMenus={navSubMenus}/>
+        <MainBox>
+            <HeaderWrapper>정치인 프로필 - 대통령</HeaderWrapper>
+            <HorizontalDivider />
+            <ContainerBox>
+                <HeadProfile>
+                    <ImageWrapper src="https://i.namu.wiki/i/mWLCLSO9VC-_L1C780b2XvYOlXGesQL3NrtsHWm2xVW1paaLWVv8DrDWd3R5iwwjWY-MapZ1rICP2-2mTwiy968UN4tOh9d2S035HaZn__4ZlXYW14-Adjz4vBF9EpfxrmYkqZ_NH_lazX1o-JpJWg.webp" alt="대통령 이미지" />
+                    <TextContainer>
+                        <NameContainer>
+                            <TextPosition>대통령</TextPosition>
+                            <TextName>문재인</TextName>
+                        </NameContainer>
+                        <ResumeContainer>
+                            { /* 이력, 경력 받을만큼 받기, 최대 8개까지, 수기로 X */}
+                            <FormContainer>
+                                <FormResume> 안녕하세요 </FormResume>
+                                <FormResume> 안녕하세요 </FormResume>
+                                <FormResume> 안녕하세요 </FormResume>
+                                <FormResume> 안녕하세요 </FormResume>
+                            </FormContainer>
+                            <FormContainer>
+                                <FormResume> 안녕하세요 </FormResume>
+                                <FormResume> 안녕하세요 </FormResume>
+                                <FormResume> 안녕하세요 </FormResume>
+                                <FormResume> 안녕하세요 </FormResume>
+                            </FormContainer>
+                        </ResumeContainer>
+                    </TextContainer>
+                </HeadProfile>
+                <HorizontalDivider />
+
+            </ContainerBox>
+        </MainBox>
+    </PageBox>
     );
 };
 
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -2,15 +2,25 @@ import styled from "styled-components";
 
 export const PageBox = styled.div`
   display: flex;
-  height: 100vh;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex: 1;
+`;
+
+export const MainBox = styled.div`
+  display: flex;
+  flex-direction: column;
   overflow-y: hidden;
+  weight: 100%;
 `;
 
 export const ContainerBox = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1.5rem;
-  padding: 0 2rem;
+  flex: 1;
+  align-items: flex-start;
+  padding: 44px 10px;
 `;
 
 export const VerticalDivider = styled.div`
@@ -23,4 +33,23 @@ export const HorizontalDivider = styled.div`
   width: 100%;
   height: 0.0625rem;
   background-color: #E4E7EC;
+`;
+
+export const HeaderWrapper = styled.div`
+  flex-direction: column;
+  display: flex;
+  font-size: 40px;
+  font-weight: 600;
+  padding: 44px 36px;
+`;
+
+export const ImageWrapper = styled.img`
+  display: flex;
+  width: 210px;
+  height: 240px;
+`;
+
+export const IconWrapper = styled.div`
+  width: 20px;
+  height: 20px;
 `;
