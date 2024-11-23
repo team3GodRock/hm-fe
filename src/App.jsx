b98cdc9918a96b6d64f781d04f3464a90f09556f
yeapy <advancedtrio@naver.com>
2024-11-23 12:01:06 +0900

    feat: add MapPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,11 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import ProfilePage from './pages/Profile/ProfilePage';
+import MapPage from './pages/Map/MapPage';
 
 function App() {
   return (
-    <RecoilRoot> {/* Recoil 상태 관리를 위한 루트 */}
-      <Router>
-        <Routes>
-          <Route path="/" element={<ProfilePage />} />
-        </Routes>
-      </Router>
-    </RecoilRoot>
+    <MapPage/>
   );
 }
 
